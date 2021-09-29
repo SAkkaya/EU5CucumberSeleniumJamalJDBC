afee2adf0681bc349bba6e380f9b1e05fe8c3d86
SAkkaya <sadikakkaya.sa@gmail.com>
2021-09-29 22:06:52 +0200

    Contacts Test db

--- src/test/java/com/vytrack/utilities/ConfigurationReader.java
+++ src/test/java/com/vytrack/utilities/ConfigurationReader.java
@@ -10,10 +10,25 @@ public class ConfigurationReader {
 
     private static Properties properties;
 
+// mvn test -Dconfig=regression
     static {
 
         try {
-            String path = "configuration.properties";
+            String path;
+            String config = System.getProperty("config");
+            if (config == null)
+                config = "default";
+            switch (config) {
+
+                case "regression":
+                    path  = "config/regressionConfig.properties";
+                    break;
+                    default:
+                    path  = "config/configuration.properties";
+            }
+
+            System.out.println("path = " + path);
+
             FileInputStream input = new FileInputStream(path);
             properties = new Properties();
             properties.load(input);
